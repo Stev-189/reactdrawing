4afad57bbc4c5b92ec436ba9c3a78a1ef45b4baa
Stev-189 <epavezllanca@gmail.com>
2021-04-05 22:24:57 -0400

    firdt look

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import CanvasDraw from "react-canvas-draw";
 
 function App() {
+  const firstCanvas= useRef(null)
+  const secondCanvas= useRef(null)
+
+  const handleClick=_=>{
+    const data= firstCanvas.current.getSaveData();
+    // console.log(data);
+    secondCanvas.current.loadSaveData(data, false);//registro a agregar, true no hay animacion solo carga, false hay animacion
+  }
+
+  const handleClear=_=>firstCanvas.current.clear()
+  const handleDeshacer=_=>firstCanvas.current.undo()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}>
+        Save Drawing
+      </button>
+      <button onClick={handleClear}>
+        Clear
+      </button>
+      <button onClick={handleDeshacer}>
+        Deshacer
+      </button>
+      <CanvasDraw 
+        brushRadius={1} 
+        brushColor={'#eccc68'} 
+        catenaryColor={"#a4b0be"} 
+        style={{border:'1px solid black'}}
+        // hideGrid={false}
+        ref={firstCanvas}
+        />
+      <CanvasDraw
+        ref={secondCanvas}
+        style={{border:'1px solid red'}}
+        hideGrid={true}
+        disabled={true}
+      />
+    </>
   );
 }
 
